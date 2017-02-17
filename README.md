95232045425ccf0fe6cb16981407f8353ff16560
vala <gonjo@free.fr>
2017-02-17 17:18:37 +0100

    small code refactorings

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Stall::Colissimo
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/stall/colissimo`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+Colissimo french shipping carrier integration with the Stall e-commerce
+framework.
 
 ## Installation
 
--- lib/stall/colissimo/calculator.rb
+++ lib/stall/colissimo/calculator.rb
@@ -4,11 +4,8 @@ module Stall
       register :colissimo
 
       def load_data
-        load_from(
-          File.read(
-            File.expand_path('../../../../vendor/colissimo.csv', __FILE__)
-          )
-        )
+        path = File.expand_path('../../../../vendor/colissimo.csv', __FILE__)
+        load_from(File.read(path))
       end
 
       def trackable?
@@ -16,8 +13,9 @@ module Stall
       end
 
       def tracking_url
-        return unless cart.shipment.tracking_code.present?
-        "http://www.colissimo.fr/portail_colissimo/suivre.do?colispart=#{ cart.shipment.tracking_code }"
+        if (tracking_code = cart.shipment.try(:tracking_code)).present?
+          "http://www.colissimo.fr/portail_colissimo/suivre.do?colispart=#{ tracking_code }"
+        end
       end
     end
   end
